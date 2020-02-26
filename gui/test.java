0055cd7f74d86cdd5ed5891cd389c2f8eadf595d
Nimra Sami <47529374+nimrasami@users.noreply.github.com>
2020-02-26 15:20:49 -0500

    GUI
    I added the set up of GUI

--- gui/test.java
+++ gui/test.java
@@ -1,11 +1,49 @@
 package gui;
 
-/** Test
+import controllers.UserController;
+
+
+/**Class to display GUI to the user
+ *
+ *
+ * Last updated February 26 2020
  *
  * @author Nimra
  */
 public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
+    // Giving access to models so they could display the information.
+    private final UserController modcontroler = new UserController();
+
+    /**
+     * Loads home screen panel.
+     */
+    public void HomePanel() {
+        
     }
-}
+    /**
+     * Loads about us panel. 
+     */
+    public void AboustUsPanel() {
+
+    }
+   /**
+     * Loads services panel
+     */
+    public void ServicesPanel() {
+
+    }
+    /**
+     * Loads support panel
+     */
+    public void SupportPanel() {
+
+    }
+    /**
+     * Loads contact us panel
+     */
+    public void ContactUsPanel() {
+
+    }
+
+    }
