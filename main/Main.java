cf746f70bde2199fe6c03e7220356a3c087992d9
Nimra Sami <47529374+nimrasami@users.noreply.github.com>
2020-03-21 00:08:23 -0400

    Fixed the main

--- main/Main.java
+++ main/Main.java
@@ -50,7 +50,7 @@ public class Main extends Application {
 
         // Creating a new user from the 'database'
         User model1 = userFromDatabase();
-        UserViewTEST view1 = new UserViewTEST();
+        UserView view1 = new UserView();
 
 
         // Creating a controller for the user
