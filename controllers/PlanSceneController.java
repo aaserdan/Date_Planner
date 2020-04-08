72befa5974f9a48216ee0e9c7eabf26fd342840c
Alejandro Penaloza <43787952+apenaloza7@users.noreply.github.com>
2020-04-08 18:43:41 -0400

    revamped plan screen with efficient options

--- controllers/PlanSceneController.java
+++ controllers/PlanSceneController.java
@@ -3,9 +3,10 @@ package controllers;
 /**
  * This class serves as the controller for the plan scene
  *
- * Last updated March 20 2020
+ * Last updated April 8 2020
  *
  * @author Nimra
+ * @author Alejandro
  */
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,72 +19,61 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class PlanSceneController implements Initializable {
 
-    //Buttons to go from plan scene to result scene
-    @FXML
-    private Button resultsButton;
-
-    //Combobox for setting options for partner one, displays all of the options
-    @FXML
-    private ComboBox<String> Partner1SettingcomboBox;
-
-    ObservableList<String> partner1Settingoptions = FXCollections.observableArrayList("Dine In", "Take Out", "Fast Food");
-
-    //Combobox for setting options for partner two, displays all of the options
-    @FXML
-    private ComboBox<String> Partner2SettingcomboBox;
-
-    ObservableList<String> partner2Settingoptions = FXCollections.observableArrayList("Dine In", "Take Out", "Fast Food");
+    // ==================================== VARIABLES ====================================
 
     //Combobox for cusine options for partner one, displays all of the options
-    @FXML
-    private ComboBox<String> Partner1CuinecomboBox;
+    @FXML private ComboBox<String> Partner1CuinecomboBox;
 
-    ObservableList<String> partner1Cusineoptions = FXCollections.observableArrayList("American","Caribbean", "Chinese", "French", "Italian", "Indian", "Japanese", "Mexican", "Mediterranean", "Spanish", "Sea Food", "Thai");
+    //Combobox for search limit options for partner one, displays all of the options
+    @FXML private ComboBox<String> Partner1SearchLimitcomboBox;
 
     //Combobox for cusine options for partner two, displays all of the options
-    @FXML
-    private ComboBox<String> Partner2CuinecomboBox;
-
-    ObservableList<String> partner2Cusineoptions = FXCollections.observableArrayList("American","Caribbean", "Chinese", "French", "Italian", "Indian", "Japanese", "Mexican", "Mediterranean", "Spanish", "Sea Food", "Thai");
-
-    //Combobox for distance options for partner one, displays all of the options
-    @FXML
-    private ComboBox<String> Partner1DistancecomboBox;
-
-    ObservableList<String> partner1Distanceoptions = FXCollections.observableArrayList("Within 5 miles", "Within 10 miles", "Within 15 miles", "Within 20 miles", "Within 50 miles", "Within 100 miles");
-
-    //Combobox for distance options for partner two, displays all of the options
-    @FXML
-    private ComboBox<String> Partner2DistancecomboBox;
-
-    ObservableList<String> partner2Distanceoptions = FXCollections.observableArrayList("Within 5 miles", "Within 10 miles", "Within 15 miles", "Within 20 miles", "Within 50 miles", "Within 100 miles");
+    @FXML private ComboBox<String> Partner2CuinecomboBox;
 
     //Combobox for search limit options for partner one, displays all of the options
-    @FXML
-    private ComboBox<String> Partner1SearchLimitcomboBox;
+    @FXML private ComboBox<String> Partner2SearchLimitcomboBox;
 
-    ObservableList<String> partner1SearchLimitoptions = FXCollections.observableArrayList("1", "2", "3", "5", "7");
+    @FXML private TextField partner1Name;
+    @FXML private TextField partner2Name;
+
+    @FXML private TextField city;
+    @FXML private TextField zipCode;
+
+    // ==================================== OBVSERVABLE LISTS ====================================
+
+    //Combobox for cusine options for partner one, displays all of the options
+    ObservableList<String> Cusineoptions = FXCollections.observableArrayList("American","Caribbean", "Chinese", "French", "Italian", "Indian", "Japanese", "Mexican", "Mediterranean", "Spanish", "Sea Food", "Thai");
 
     //Combobox for search limit options for partner one, displays all of the options
-    @FXML
-    private ComboBox<String> Partner2SearchLimitcomboBox;
+    ObservableList<String> SearchLimitoptions = FXCollections.observableArrayList("1", "2", "3", "5", "7");
 
-    ObservableList<String> partner2SearchLimitoptions = FXCollections.observableArrayList("1", "2", "3", "5", "7");
+    // ==================================== METHODS/ACTIONS ====================================
 
     /**
-     * Takes the user from plan screen to results screen
+     * Gathers data and takes the user from plan screen to results screen
      *
      * @param _event
      * @throws java.lang.Exception
      */
     @FXML
-    public void changeToResultsScreen(ActionEvent _event) throws Exception {
+    public void resultsButtonPressed(ActionEvent _event) throws Exception {
+
+        String partnerOneName = this.partner1Name.getText();
+        String partnerTwoName = this.partner2Name.getText();
+        String cityGiven = this.city.getText();
+        String zipCodeGiven = this.zipCode.getText();
+
+        System.out.println("Switching to results screen...");
+        System.out.println(partnerOneName);
+        System.out.println(partnerTwoName);
+        System.out.println(cityGiven);
+        System.out.println(zipCodeGiven);
 
         //new FXML loader and scene for new screen
         Parent root = FXMLLoader.load(getClass().getResource("/views/ResultsScene.fxml"));
@@ -103,14 +93,10 @@ public class PlanSceneController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Partner1SettingcomboBox.setItems(partner1Settingoptions);
-        Partner2SettingcomboBox.setItems(partner2Settingoptions);
-        Partner1CuinecomboBox.setItems(partner1Cusineoptions);
-        Partner2CuinecomboBox.setItems(partner2Cusineoptions);
-        Partner1DistancecomboBox.setItems(partner1Distanceoptions);
-        Partner2DistancecomboBox.setItems(partner2Distanceoptions);
-        Partner1SearchLimitcomboBox.setItems(partner1SearchLimitoptions);
-        Partner2SearchLimitcomboBox.setItems(partner2SearchLimitoptions);
+        Partner1CuinecomboBox.setItems(Cusineoptions);
+        Partner2CuinecomboBox.setItems(Cusineoptions);
+        Partner1SearchLimitcomboBox.setItems(SearchLimitoptions);
+        Partner2SearchLimitcomboBox.setItems(SearchLimitoptions);
     }
 
 }
