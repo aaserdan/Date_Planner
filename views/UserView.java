7bcf01ec3a64debc2bb1e9768e6fe8cc534d935b
Alejandro Penaloza <43787952+apenaloza7@users.noreply.github.com>
2020-04-08 18:44:52 -0400

    removed country code

--- views/UserView.java
+++ views/UserView.java
@@ -17,17 +17,15 @@ public class UserView {
      * @param _city city given by user
      * @param _term term given by user ('pasta', 'seafood', etc.)
      * @param _zipCode zip code given by user
-     * @param _countryCode country code given by user (ex. 'us')
      * @param _searchLimitYelp search limit given by user
      */
-    public void printResults(String _name, String _city, String _term, int _zipCode, String _countryCode, int _searchLimitYelp) {
+    public void printResults(String _name, String _city, String _term, int _zipCode, int _searchLimitYelp) {
         System.out.println("Data given: ");
 
         System.out.println("User: " + _name);
         System.out.println("City: " + _city);
         System.out.println("Term Used: " + _term);
         System.out.println("Zip Code: " + _zipCode);
-        System.out.println("Country Code: " + _countryCode);
         System.out.println("Search Limit: " + _searchLimitYelp);
 
         // Here to seperate the data given and the results of the program
