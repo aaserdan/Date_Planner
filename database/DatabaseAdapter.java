911d30c15ae98da8906fb38b5a3d0d6d9c52a314
Arturo Serdan <aaserdan@uncg.edu>
2020-05-02 21:16:19 -0400

    Added all CRUD operations to Database,DatabaseInterface, and DatabaseAdapter

--- database/DatabaseAdapter.java
+++ database/DatabaseAdapter.java
@@ -2,11 +2,14 @@ package database;
 
 /**
  * Database Adapter class used to make calls to the Database 
- * Last updated April 13, 2020
+ * Last updated May 2, 2020
  * @Author Arturo
  */
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class DatabaseAdapter {
@@ -56,6 +59,23 @@ public class DatabaseAdapter {
     }
     
     /**
+     * Finds the data in a file based on the UUID, returns a string containing the data
+     * @param _file file to be read from
+     * @param _uuid UUID String
+     * @return element Returns the data contained at the location of the UUID
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public static String findText(File _file, String _uuid) throws FileNotFoundException, IOException{
+        // String where the elements found will be stored
+        String element = "";
+        // Instanciates findText method from Database, then calls method in Database
+        Database findText = new Database();
+        element = findText.findText(_file, _uuid);
+        return element;
+    }
+    
+    /**
      * Calls getLastElements to get the last "x" elements from file
      * @param _file Elements extracted from 
      * @param _numElmtsReturn Number of elements to return
@@ -69,5 +89,65 @@ public class DatabaseAdapter {
         //Passes _file and _numElmtsReturn to method in Database
         elmtsInFile = getLastElement.getLastElements(_file, _numElmtsReturn);
         return elmtsInFile;
+    }
+    
+    /**
+     * This method creates a temporal file, then passes all the data from the original file to the temporary file
+     * except for the element that is wished to be deleted, then deletes the "original" and renames the temporary as 
+     * the original.
+     * @param _file the file from which an element will be deleted
+     * @param _UUID UUID String
+     * @throws IOException 
+     */
+    public static void removeRecordByUUID(File _file, String _UUID) throws IOException{
+        //Creates a new instance of removeRecordByUUID from Database class
+        Database removeRecordByUUID = new Database();
+        //Passes _file and _UUID to method in Database
+        removeRecordByUUID.removeRecordByUUID(_file, _UUID);
+    }
+    
+    /**
+     * This method will search by a key element in the File, and return all the UUIDs that match the key
+     * in an Array String
+     * @param _file
+     * @param _keyString
+     * @return mathchesFound with the UUIDs found to match the elements
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public static String [] findbyKeyInFile(File _file, String _keyString) throws FileNotFoundException, IOException{
+        //Creates a new instance of findbyKeyInFile from Database class
+        Database findbyKeyInFile = new Database();
+        //Passes _file and _keyString to method in Database
+        String [] foundMatches = findbyKeyInFile.findbyKeyInFile(_file, _keyString);
+        return foundMatches;
+    }
+    
+    /**
+     * This method updates a element given a file, a UUID, and a Array String containing the new data
+     * @param _file File to be updated
+     * @param _UUID UUID of the element/data to be changed
+     * @param _data New data that will be placed
+     * @throws IOException 
+     */
+    public static void updateElement(File _file, String _UUID, String [] _data) throws IOException{
+        //Creates a new instance of updateElement from Database class
+        Database updateElement = new Database();
+        //Passes _file, _UUID, _data to method in Database
+        updateElement.updateElement(_file, _UUID, _data);
+    }
+    
+    /**
+     * Method to create a new text file given a file name, it will create file in the Database package
+     * @param _fileName Name of the file, no need of ".txt" 
+     * @return true if file created, else false
+     */
+    public static boolean makeNewFile(String _fileName){
+        boolean response = false;
+        // Creates an instance of makeNewFile method in Database
+        Database makeNewFile = new Database();
+        // Passes_fileName to method in database
+        response = makeNewFile.makeNewFile(_fileName);
+        return response;
     }
 }
--- database/DatabaseInterface.java
+++ database/DatabaseInterface.java
@@ -1,114 +1,133 @@
 package database;
 
-/**
- * Provides a way to write and read from a file
- *
- * @author Arturo Last updated 4/13/2020
- */
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Provides a way to write in a file as well as reading from it
+ *
+ * @author Arturo Last updated 4/1/2020
+ */
 public interface DatabaseInterface {
 
     // Used to keep track of the UUIDs and store them
     static Stack<String> stack = new Stack<String>();
-    
+
     // Number of the UUIDs
     static int fileSize = 0;
     static int iterator = 0;
-    
+
     /**
-     * Writes in elements on a file, writing a coma to separate different information
-     * Provides a UUID to keep track of elements written
+     * Writes in elements on a file, writing a coma to separate different
+     * information Provides a UUID to keep track of elements written
+     *
      * @param _array elements coming in to be written
      * @param _file file where the information will be stored
      */
-    void writeIn(String [] _array, File _file);
+    void writeIn(String[] _array, File _file);
 
     /**
      * Reads the entire file and puts the content on a String to be returned
+     *
      * @param _file file to be read from
      * @return fullFile String that contains the data from file
      * @throws FileNotFoundException
-     * @throws IOException 
+     * @throws IOException
      */
     String readFile(File _file) throws FileNotFoundException, IOException;
-    
+
     /**
-     * Finds the data in a file based on the UUID, returns a string containing the data
+     * Finds the data in a file based on the UUID, returns a string containing
+     * the data
+     *
      * @param _file file to be read from
-     * @param _uuid UUID String 
+     * @param _uuid UUID String
      * @return element Returns the data contained at the location of the UUID
      * @throws FileNotFoundException
-     * @throws IOException 
+     * @throws IOException
      */
     String findText(File _file, String _uuid) throws FileNotFoundException, IOException;
-    
+
     /**
-     * Reads and saves all the UUIDs contained on a file, adds them to ReadInFile.stack, and returns the number of UUIDs/elements found
-     * If file is empty returns 0
+     * Reads and saves all the UUIDs contained on a file, adds them to
+     * ReadInFile.stack, and returns the number of UUIDs/elements found If file
+     * is empty returns 0
+     *
      * @param _file the file where the size will be read from
      * @return ReadInFile.fileSize with the size from the ReadInFile.stack
-     * @throws IOException 
+     * @throws IOException
      */
-    int getSizFile(File _file) throws IOException; 
-     
+    int getSizFile(File _file) throws IOException;
+
     /**
-     * Gets the last "x" number of elements from a file, and returns them on an Array String
+     * Gets the last "x" number of elements from a file, and returns them on an
+     * Array String
+     *
      * @param _file the file that is be read from
      * @param _numElmtsReturn number of elements to be returned
      * @return elmtsInFile Array String with the elements from the file
-     * @throws IOException 
+     * @throws IOException
      */
-    String [] getLastElements(File _file, int _numElmtsReturn) throws IOException;
-    
+    String[] getLastElements(File _file, int _numElmtsReturn) throws IOException;
+
     /**
      * Finds the last "x" number of UUIDs on the file
+     *
      * @param _numElmtsReturn number of elements to be returned
-     * @return elementsReturned Array string containing the last "x" number of UUIDS from the file 
+     * @return elementsReturned Array string containing the last "x" number of
+     * UUIDS from the file
      */
-    String [] getLastUUID(File _file, int _numUUIDsReturn) throws IOException;
-    
-    // -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    // Methods and elements needed to generate a UUID for the Database to use
-    
-    // Characters to be used on to generate the UUID
-    final char[] sourceCharacters = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', '0','1','2','3','4','5','6','7','8','9'};
-    
-    // UUID parts dived by number of characters
-    final int timeLow = 8;
-    final int timeMid = 4;
-    final int timeHiAndVersion = 4;
-    final int clockSeqHiAndResClockSeqLow = 4;
-    final int node =12;
-    
+    String[] getLastUUID(File _file, int _numUUIDsReturn) throws IOException;
+
     /**
-     * Generates a UUID 
-     * @return UUIDreturned String returned containing the UUID
+     * This method will search by a key element in the File, and return all the
+     * UUIDs that match the key in an Array String
+     *
+     * @param _file
+     * @param _keyString
+     * @return mathchesFound with the UUIDs found to match the elements
+     * @throws FileNotFoundException
+     * @throws IOException
      */
-    String generateUUID();
-    
+    String[] findbyKeyInFile(File _file, String _keyString) throws FileNotFoundException, IOException;
+
     /**
-     * Generates the random parts of the UUID
-     * @param strLength length of the String to be created
-     * @return randomString randomString to be returned
+     * This method creates a temporal file, then passes all the data from the
+     * original file to the temporary file except for the element that is wished
+     * to be deleted, then deletes the "original" and renames the temporary as
+     * the original.
+     *
+     * @param _file the file from which an element will be deleted
+     * @param _UUID UUID String
+     * @throws IOException
      */
-    String getRandomString(int strLength);
-    
+    void removeRecordByUUID(File _file, String _UUID) throws IOException;
+
     /**
-     * Returns a random character from the list of characters
-     * @return the random character
+     * This method updates a element given a file, a UUID, and a Array String
+     * containing the new data
+     *
+     * @param _file File to be updated
+     * @param _UUID UUID of the element/data to be changed
+     * @param _data New data that will be placed
+     * @throws IOException
      */
-    public static char getRandomChar() {
-        int rand = ThreadLocalRandom.current().nextInt(0,DatabaseInterface.sourceCharacters.length);        
-        char chrReturned = DatabaseInterface.sourceCharacters[rand];
-        return chrReturned;
-    }
+    public void updateElement(File _file, String _UUID, String[] _data) throws IOException;
+
+    /**
+     * Method to create a new text file given a file name, it will create file
+     * in the Database package
+     *
+     * @param _fileName Name of the file, no need of ".txt"
+     * @return true if file created, else false
+     */
+    public boolean makeNewFile(String _fileName);
 }
+
